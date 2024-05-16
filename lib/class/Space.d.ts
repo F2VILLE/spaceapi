c2ec1177af28bcf496a246e4a2568a9ca836ff88
f2ville <F2Ville>
2024-05-16 16:55:47 +0200

    Adding getters in Space class, and findByName function to search a space's API link from the repo

--- lib/class/Space.d.ts
+++ lib/class/Space.d.ts
@@ -1,17 +1,24 @@
-import SpaceAPISchema from '../types/SpaceAPISchema';
+import SpaceAPISchema from "../types/SpaceAPISchema";
 declare class Space {
     url: string;
     data: SpaceAPISchema;
     constructor(url: string);
     fetch(): Promise<SpaceAPISchema>;
+    static findByName(name: string): Promise<Array<{
+        name: string;
+        url: string;
+    }>>;
     get name(): string;
-    get contact(): SpaceAPISchema['contact'];
-    get location(): SpaceAPISchema['location'];
-    get state(): SpaceAPISchema['state'];
-    get events(): SpaceAPISchema['events'];
-    get sensors(): SpaceAPISchema['sensors'];
-    get feeds(): SpaceAPISchema['feeds'];
+    get contact(): SpaceAPISchema["contact"];
+    get location(): SpaceAPISchema["location"];
+    get stateOpen(): boolean;
+    get stateOpenFull(): SpaceAPISchema["state"];
+    get events(): SpaceAPISchema["events"];
+    get projects(): SpaceAPISchema["projects"];
+    get sensors(): SpaceAPISchema["sensors"];
+    get feeds(): SpaceAPISchema["feeds"];
     get logo(): string;
+    get stateLogo(): string;
     get space(): SpaceAPISchema["space"];
 }
 export default Space;
--- src/class/Space.ts
+++ src/class/Space.ts
@@ -1,60 +1,116 @@
-import axios from 'axios'
-import SpaceAPISchema from '../types/SpaceAPISchema'
+import axios from "axios";
+import SpaceAPISchema from "../types/SpaceAPISchema";
 
 class Space {
-    data: SpaceAPISchema
-    constructor(public url: string) {
-        this.data = null
-    }
+  data: SpaceAPISchema;
+  constructor(public url: string) {
+    this.data = null;
+  }
 
-    async fetch(): Promise<SpaceAPISchema> {
-        return new Promise((resolve, reject) => {
-            axios.get(this.url)
-                .then((response) => {
-                    this.data = response.data
-                    resolve(response.data)
-                })
-                .catch((error) => {
-                    reject(error)
-                })
+  async fetch(): Promise<SpaceAPISchema> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(this.url)
+        .then((response) => {
+          this.data = response.data;
+          resolve(response.data);
         })
-    }
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
 
-    get name(): string {
-        return this.data.space
-    }
+  static async findByName(
+    name: string
+  ): Promise<Array<{ name: string; url: string }>> {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(
+          "https://raw.githubusercontent.com/SpaceApi/directory/master/directory.json"
+        )
+        .then((response) => {
+          const spaces = response.data;
 
-    get contact(): SpaceAPISchema['contact'] {
-        return this.data.contact
-    }
+          const spacesFound = Object.keys(spaces).filter((key) =>
+            key
+              .toLowerCase()
+              .replace(/\s+/gm, "")
+              .includes(name.toLowerCase().replace(/\s+/gm, ""))
+          );
 
-    get location(): SpaceAPISchema['location'] {
-        return this.data.location
-    }
+          if (!spacesFound.length) return resolve(null);
 
-    get state(): SpaceAPISchema['state'] {
-        return this.data.state
-    }
+          resolve(
+            spacesFound.map((key) => {
+              return {
+                name: key,
+                url: spaces[key],
+              };
+            })
+          );
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
 
-    get events(): SpaceAPISchema['events'] {
-        return this.data.events
-    }
+  get name(): string {
+    return this.data.space;
+  }
 
-    get sensors(): SpaceAPISchema['sensors'] {
-        return this.data.sensors
-    }
+  get contact(): SpaceAPISchema["contact"] {
+    return this.data.contact;
+  }
 
-    get feeds(): SpaceAPISchema['feeds'] {
-        return this.data.feeds
-    }
+  get location(): SpaceAPISchema["location"] {
+    return this.data.location;
+  }
 
-    get logo(): string {
-        return this.data.logo
-    }
+  get stateOpen(): boolean {
+    return this.data.state.open;
+  }
 
-    get space(): SpaceAPISchema["space"] {
-        return this.data.space
-    }
+  get stateOpenFull(): SpaceAPISchema["state"] {
+    return this.data.state;
+  }
+
+  get events(): SpaceAPISchema["events"] {
+    return this.data.events;
+  }
+
+  get projects(): SpaceAPISchema["projects"] {
+    return this.data.projects;
+  }
+
+  get sensors(): SpaceAPISchema["sensors"] {
+    return this.data.sensors;
+  }
+
+  get feeds(): SpaceAPISchema["feeds"] {
+    return this.data.feeds;
+  }
+
+  get logo(): string {
+    return this.data.logo;
+  }
+
+  get stateLogo(): string {
+    if (
+      !this.data.state?.icon ||
+      !this.data.state.icon.open ||
+      !this.data.state.icon.closed
+    )
+      return null;
+    return this.data.state.open
+      ? this.data.state.icon.open
+      : this.data.state.icon.closed;
+  }
+
+  get space(): SpaceAPISchema["space"] {
+    return this.data.space;
+  }
 }
 
-export default Space
+export default Space;
